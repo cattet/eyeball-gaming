651cfff87084079b52fb66296c8a3898a57fd79c
pickol <saki@cakeforge.co>
2025-02-13 03:20:59 -0800

    party lineup logic and toggling

--- gaming/src/app/home/home.component.ts
+++ gaming/src/app/home/home.component.ts
@@ -2,8 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { xivNumberPipe } from './pipes/xivnumber.pipe';
 import { xivDecimalPipe } from './pipes/xivdecimal.pipe';
@@ -26,7 +27,8 @@ export interface player {
   manaPercent: number;
   aggroPercent: number;
   aggroOrder: number;
-  group: number;
+  jobPriority: number;
+  groupPriority: number;
   lineUpOrder: number;
   debuffs: debuff[];
 }
@@ -37,83 +39,91 @@ export interface debuff {
   duration: string;
 }
 
+const basePlayerData: player[] = [
+  { id: 0, job: 'MCH', level: 90, name: 'Range Group1', maxHealth: 66791, healthPercent: 1, manaPercent: 1, aggroPercent: 0.2, aggroOrder: 6, jobPriority: 2, groupPriority: 2, lineUpOrder: 0, debuffs: [] },
+  { id: 1, job: 'WAR', level: 90, name: 'Tank Group1', maxHealth: 94171, healthPercent: 0.8, manaPercent: 1, aggroPercent: 0.8, aggroOrder: 2, jobPriority: 4, groupPriority: 2, lineUpOrder: 0, debuffs: [] },
+  { id: 2, job: 'PLD', level: 90, name: 'Tank Group2', maxHealth: 96236, healthPercent: 0.65, manaPercent: 1, aggroPercent: 1, aggroOrder: 1, jobPriority: 4, groupPriority: 1, lineUpOrder: 0, debuffs: [] },
+  { id: 3, job: 'WHM', level: 90, name: 'Healer Group1', maxHealth: 61056, healthPercent: 1, manaPercent: .777, aggroPercent: 0.1, aggroOrder: 7, jobPriority: 3, groupPriority: 2, lineUpOrder: 0, debuffs: [] },
+  { id: 4, job: 'SCH', level: 90, name: 'Healer Group2', maxHealth: 61056, healthPercent: 1, manaPercent: .95, aggroPercent: 0.1, aggroOrder: 8, jobPriority: 3, groupPriority: 1, lineUpOrder: 0, debuffs: [] },
+  { id: 5, job: 'DRG', level: 90, name: 'Melee Group1', maxHealth: 67602, healthPercent: 1, manaPercent: 1, aggroPercent: 0.3, aggroOrder: 4, jobPriority: 1, groupPriority: 2, lineUpOrder: 0, debuffs: [] },
+  { id: 6, job: 'SMN', level: 90, name: 'Melee Group2', maxHealth: 58116, healthPercent: 1, manaPercent: .96, aggroPercent: 0.2, aggroOrder: 5, jobPriority: 1, groupPriority: 1, lineUpOrder: 0, debuffs: [] },
+  { id: 7, job: 'PCT', level: 90, name: 'Range Group2', maxHealth: 61081, healthPercent: 1, manaPercent: .95, aggroPercent: 0.4, aggroOrder: 3, jobPriority: 2, groupPriority: 1, lineUpOrder: 0, debuffs: [] }
+]
+
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, MatGridListModule, MatCardModule, MatToolbarModule, MatButtonModule, MatProgressBarModule, CdkDropListGroup, CdkDropList, CdkDrag, xivNumberPipe, xivDecimalPipe ],
+  imports: [CommonModule, MatGridListModule, MatCardModule, MatToolbarModule, MatButtonModule, MatIconModule, MatProgressBarModule, CdkDropListGroup, CdkDropList, CdkDrag, xivNumberPipe, xivDecimalPipe ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
 
-  public playerData: player[] = [
-    { id: 0, job: 'MCH', level: 90, name: 'Range Group1', maxHealth: 66791, healthPercent: 1, manaPercent: 1, aggroPercent: 0.2, aggroOrder: 6, group: 1, lineUpOrder: 0, debuffs: [ {id: 2, name: 'spread', iconUrl: '/assets/spread.png', duration: '42' }, {id: 1, name: 'passed-vow', iconUrl: '/assets/passed-vow.png', duration: '1m'} ] },
-    { id: 1, job: 'WAR', level: 90, name: 'Tank Group1', maxHealth: 94171, healthPercent: 0.8, manaPercent: 1, aggroPercent: 0.8, aggroOrder: 2, group: 1, lineUpOrder: 0, debuffs: [ {id: 0, name: 'vow', iconUrl: '/assets/vow.png', duration: '27' } ] },
-    { id: 2, job: 'PLD', level: 90, name: 'Tank Group2', maxHealth: 96236, healthPercent: 0.65, manaPercent: 1, aggroPercent: 1, aggroOrder: 1, group: 2, lineUpOrder: 0, debuffs: [ {id: 2, name: 'spread', iconUrl: '/assets/spread.png', duration: '42' } ] },
-    { id: 3, job: 'WHM', level: 90, name: 'Healer Group1', maxHealth: 61056, healthPercent: 1, manaPercent: .777, aggroPercent: 0.1, aggroOrder: 7, group: 1, lineUpOrder: 0, debuffs: [ {id: 3, name: 'stack', iconUrl: '/assets/stack.png', duration: '42' } ] },
-    { id: 4, job: 'SCH', level: 90, name: 'Healer Group2', maxHealth: 61056, healthPercent: 1, manaPercent: .95, aggroPercent: 0.1, aggroOrder: 8, group: 2, lineUpOrder: 0, debuffs: [ {id: 2, name: 'spread', iconUrl: '/assets/spread.png', duration: '42' } ] },
-    { id: 5, job: 'DRG', level: 90, name: 'Melee Group1', maxHealth: 67602, healthPercent: 1, manaPercent: 1, aggroPercent: 0.3, aggroOrder: 4, group: 1, lineUpOrder: 0, debuffs: [ {id: 2, name: 'spread', iconUrl: '/assets/spread.png', duration: '42' } ] },
-    { id: 6, job: 'SMN', level: 90, name: 'Melee Group2', maxHealth: 58116, healthPercent: 1, manaPercent: .96, aggroPercent: 0.2, aggroOrder: 5, group: 2, lineUpOrder: 0, debuffs: [] },
-    { id: 7, job: 'PCT', level: 90, name: 'Range Group2', maxHealth: 61081, healthPercent: 1, manaPercent: .95, aggroPercent: 0.4, aggroOrder: 3, group: 2, lineUpOrder: 0, debuffs: [ {id: 3, name: 'stack', iconUrl: '/assets/stack.png', duration: '42' } ] }
+  public wrothDebuffs: debuff[] = [
+    {id: 0, name: 'spread', iconUrl: '/assets/spread.png', duration: '23' },
+    {id: 0, name: 'spread', iconUrl: '/assets/spread.png', duration: '23' },
+    {id: 0, name: 'spread', iconUrl: '/assets/spread.png', duration: '23' },
+    {id: 0, name: 'spread', iconUrl: '/assets/spread.png', duration: '23' },
+    {id: 1, name: 'stack', iconUrl: '/assets/stack.png', duration: '23' },
+    {id: 1, name: 'stack', iconUrl: '/assets/stack.png', duration: '23' }
   ]
+
+  public miscDebuffs: debuff[] = [
+    {id: 2, name: 'passed-vow', iconUrl: '/assets/passed-vow.png', duration: '1m'},
+    {id: 3, name: 'vow', iconUrl: '/assets/vow.png', duration: '30' }
+  ]
+
+  public partyList: player[] = [];
   public solvedPlayers: player[] = [];
+  public showAnswer: boolean = false;
 
   ngOnInit() {
-    this.assignSpots();
+    this.shuffleDebuffs();
   }
 
-  assignSpots(): void {
-    this.solvedPlayers = this.playerData.slice();
-    this.shuffle(this.solvedPlayers);
-    console.log(this.solvedPlayers);
+  shuffleDebuffs(): void {
+    this.assignDebuffs();
+    this.solveSpots();
   }
 
-  shuffle(array: player[]): void {
-    let currentIndex = array.length;
-  
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  }
+  assignDebuffs(): void {
+    // Get a fresh copy of player data
+    this.partyList = this.clone(basePlayerData);
+    var workingPlayerIds: number[] = this.partyList.map(p => p.id);
+   
+    this.wrothDebuffs.forEach(debuff => {
+      // Take a random player from our working players list
+      const index = Math.floor(Math.random() * workingPlayerIds.length);
+      var luckyPlayerId: number = workingPlayerIds[index];
 
-  calculateSpots(): void {
-    this.playerData.forEach(player => {
-      switch(player.lineUpOrder){
-        case 1: 
-          this.solvedPlayers[1] = player;
-          break;
-        case 2:
-          this.solvedPlayers[2] = player;
-          break;
-        case 3:
-          this.solvedPlayers[3] = player;
-          break;
-        case 4:
-          this.solvedPlayers[4] = player;
-          break;
-        case 5:
-          this.solvedPlayers[5] = player;
-          break;
-        case 6:
-          this.solvedPlayers[6] = player;
-          break;
-        case 7:
-          this.solvedPlayers[7] = player;
-          break;
-        case 8:
-          this.solvedPlayers[8] = player;
-          break;
-        default:
-          break;
+      // Copy debuff into actual player data
+      var partyPlayer = this.partyList.find(p => p.id == luckyPlayerId);
+      if(partyPlayer) {
+        partyPlayer.debuffs = [debuff];
       }
+
+      // Remove the player from our working array to assign debuffs to other players only
+      workingPlayerIds.splice(index, 1);
     });
+  }
+
+  solveSpots(){
+    var allPlayers: player[] = this.clone(this.partyList);
+    var spreadPlayers: player[] =  allPlayers.filter(p => p.debuffs.find(d => d.name == 'spread'));
+    var stackPlayers: player[] =  allPlayers.filter(p => p.debuffs.find(d => d.name == 'stack'));
+    var nothingPlayers: player[] =  allPlayers.filter(p => !p.debuffs.find(d => d.name == 'spread') && !p.debuffs.find(d => d.name == 'stack'));
+
+    spreadPlayers = this.sortPlayersByPriority(spreadPlayers);
+    stackPlayers = this.sortPlayersByPriority(stackPlayers);
+    nothingPlayers = this.sortPlayersByPriority(nothingPlayers);
+    
+    this.solvedPlayers = [];
+    this.solvedPlayers = this.solvedPlayers.concat(spreadPlayers);
+    this.solvedPlayers.push(stackPlayers[0]);
+    this.solvedPlayers.push(nothingPlayers[0]);
+    this.solvedPlayers.push(stackPlayers[1]);
+    this.solvedPlayers.push(nothingPlayers[1]);
+
+    console.log(this.solvedPlayers);
   }
 
   drop(event: CdkDragDrop<player[]>) {
@@ -127,6 +137,20 @@ export class HomeComponent implements OnInit {
         event.currentIndex,
       );
     }
+  }
+
+  sortPlayersByPriority(players: player[]): player[] {
+    return players.sort((a, b) => {
+      if (a.jobPriority > b.jobPriority) return 1;
+      if (a.jobPriority < b.jobPriority) return -1;
+      if (a.groupPriority > b.groupPriority) return 1;
+      if (a.groupPriority < b.groupPriority) return -1;
+      return 0;
+    });
+  }
+
+  clone(arrayToClone: any): any {
+    return JSON.parse(JSON.stringify(arrayToClone)) as typeof arrayToClone;
   }
 
 }
