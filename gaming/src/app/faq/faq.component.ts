521857ab8d6696e7f8af1a7dafae9e36d8b06862
pickol <saki@cakeforge.co>
2025-02-16 05:28:26 -0800

    cooler faq buttons

--- gaming/src/app/faq/faq.component.ts
+++ gaming/src/app/faq/faq.component.ts
@@ -1,11 +1,12 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MatDialogModule} from '@angular/material/dialog';
 import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-faq',
-  imports: [MatButtonModule, MatDialogModule, MatChipsModule],
+  imports: [MatButtonModule, MatDialogModule, MatChipsModule, MatIconModule],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.scss'
 })
